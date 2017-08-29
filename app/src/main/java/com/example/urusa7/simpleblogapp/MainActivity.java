32ffc498281fe47cf26d7cb35a4e732a92ce88a9
urusa7 <abt737@gmail.com>
2017-08-28 18:26:54 -0700

    Initial commit

---
+++ app/src/main/java/com/example/urusa7/simpleblogapp/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.urusa7.simpleblogapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    //call main menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+
+         getMenuInflater().inflate(R.menu.main_menu, menu);
+
+         return super.onCreateOptionsMenu(menu);
+
+    }
+}
